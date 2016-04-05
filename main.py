25e1eb189249fa44f419fa7df45a1be2b89617ce
Pang Jen <Hung>
2016-04-05 10:07:46 +0800

    HPJ local

--- main.py
+++ main.py
@@ -2,21 +2,18 @@ import scipy.io
 import numpy
 import time
 
-TRACE_PATH = './20160304/20160304-0001'
-PLAIN_TEXT_PATH = './20160304/log20160304-0001.txt'
+TRACE_PATH_1 = './20160304/20160304-0001'
+PLAIN_TEXT_PATH_1 = './20160304/log20160304-0001.txt'
 TRACE_PATH_2 = './20160304/20160304-0002'
 PLAIN_TEXT_PATH_2 = './20160304/log20160304-0002.txt'
 LOG = "LOG.txt"
 TRACE_NAME = 'A'
 
-CS_TIME = time.time()
-BYTE_AMOUNT = 16 # 0~15
-KEY_AMOUNT = 256 # 00~FF
-DATA_AMOUNT = 2500*2 # old is 2500
+BYTE_AMOUNT = 16  # 16
+KEY_AMOUNT = 256  # 00~FF
+DATA_AMOUNT = 2500*2  # 2500*2
 TRACE_AMOUNT = 100004
-traceList = []
-plainTextList = []
-junk = ""
+
 SBOX = [99, 124, 119, 123, 242, 107, 111, 197, 48, 1, 103, 43, 254, 215, 171, 118, 202, 130, 201, 125,
         250, 89, 71, 240, 173, 212, 162, 175, 156, 164, 114, 192, 183, 253, 147, 38, 54, 63, 247, 204,
         52, 165, 229, 241, 113, 216, 49, 21, 4, 199, 35, 195, 24, 150, 5, 154, 7, 18, 128, 226,
@@ -31,79 +28,91 @@ SBOX = [99, 124, 119, 123, 242, 107, 111, 197, 48, 1, 103, 43, 254, 215, 171, 11
         134, 193, 29, 158, 225, 248, 152, 17, 105, 217, 142, 148, 155, 30, 135, 233, 206, 85, 40, 223,
         140, 161, 137, 13, 191, 230, 66, 104, 65, 153, 45, 15, 176, 84, 187, 22]
 
-
-def getY( plainText, nByte, key ):
+def getY(plainText, nByte, key):
     x = plainText[nByte*2:nByte*2+2]
-    return SBOX[ int(x,16) ^ key ]
+    return SBOX[int(x, 16) ^ key]
 
-for i in range(DATA_AMOUNT//2):
-    fileName= '%s%s_%04d' % ( TRACE_PATH, TRACE_PATH[len( TRACE_PATH )-14:], i+1 )
-    traceList.append( scipy.io.loadmat( fileName ) )
-for i in range(DATA_AMOUNT//2):
-    fileName = '%s%s_%04d' % ( TRACE_PATH_2, TRACE_PATH_2[len( TRACE_PATH_2 ) - 14:], i + 1 )
-    traceList.append( scipy.io.loadmat( fileName ) )
+def hammingWeight(input):
+    count = 0
+    if input & 0x80:
+        count += 1
+    if input & 0x40:
+        count += 1
+    if input & 0x20:
+        count += 1
+    if input & 0x10:
+        count += 1
+    if input & 0x08:
+        count += 1
+    if input & 0x04:
+        count += 1
+    if input & 0x02:
+        count += 1
+    if input & 0x01:
+        count += 1
+    return count
 
 
-plainTextFile = open(PLAIN_TEXT_PATH, 'r')
-for i in range( DATA_AMOUNT//2 ):
-    dataTemp = plainTextFile.readline()
-    plainTextList.append( dataTemp[len( dataTemp )-33:len( dataTemp )-1] )
-    junk = plainTextFile.readline()
-plainTextFile.close()
+lsb0_count = numpy.zeros((BYTE_AMOUNT, KEY_AMOUNT), dtype=int)
+lsb1_count = numpy.zeros((BYTE_AMOUNT, KEY_AMOUNT), dtype=int)
+lsb0_avg = numpy.zeros((BYTE_AMOUNT, KEY_AMOUNT, TRACE_AMOUNT, 1), dtype=float)
+lsb1_avg = numpy.zeros((BYTE_AMOUNT, KEY_AMOUNT, TRACE_AMOUNT, 1), dtype=float)
+sub_lsb1_lsb0 = numpy.zeros((BYTE_AMOUNT, KEY_AMOUNT, TRACE_AMOUNT, 1), dtype=float)
 
+start_time = time.time()
+plainTextFile1 = open(PLAIN_TEXT_PATH_1, 'r')
 plainTextFile2 = open(PLAIN_TEXT_PATH_2, 'r')
-for i in range( DATA_AMOUNT//2 ):
-    dataTemp = plainTextFile2.readline()
-    plainTextList.append( dataTemp[len( dataTemp )-33:len( dataTemp )-1] )
-    junk = plainTextFile2.readline()
-plainTextFile2.close()
+
+for i in range(DATA_AMOUNT):
+
+    log_key = ''
+    print_key = ''
+
+    if i//2500 == 0:
+        fileName = '%s%s_%04d' % (TRACE_PATH_1, TRACE_PATH_1[len(TRACE_PATH_1)-14:], i+1)
+        trace_now = scipy.io.loadmat(fileName)[TRACE_NAME]
+
+        dataTemp = plainTextFile1.readline()
+        plainText = dataTemp[len(dataTemp) - 33:len(dataTemp) - 1]
+        junk = plainTextFile1.readline()
+
+    else:
+        fileName = '%s%s_%04d' % (TRACE_PATH_2, TRACE_PATH_2[len(TRACE_PATH_2) - 14:], i + 1)
+        trace_now = scipy.io.loadmat(fileName)[TRACE_NAME]
+
+        dataTemp = plainTextFile2.readline()
+        plainText = dataTemp[len(dataTemp) - 33:len(dataTemp) - 1]
+        junk = plainTextFile2.readline()
 
 
-LSB0_Count = [0.0]*KEY_AMOUNT*BYTE_AMOUNT
-LSB1_Count = [0.0]*KEY_AMOUNT*BYTE_AMOUNT
-LSB0_Avg = [0.0]*KEY_AMOUNT*BYTE_AMOUNT
-LSB1_Avg = [0.0]*KEY_AMOUNT*BYTE_AMOUNT
-SUB_LSB1Avg_LSB0Avg = [0.0]*KEY_AMOUNT*BYTE_AMOUNT
-keyTemp = [0.0]*KEY_AMOUNT*BYTE_AMOUNT
+    for nByte in range(BYTE_AMOUNT):
 
-S_time = time.time()
+        maxPoint = 0.0
+        maxKey = 0
 
-for i in range( DATA_AMOUNT ):
+        for key in range(KEY_AMOUNT):
 
-    log_KEY = ""
-    print_KEY = ""
-
-    for nByte in range( BYTE_AMOUNT ):
-        for key in range( KEY_AMOUNT ):
-            if getY(plainTextList[i], nByte, key) % 2 == 0:
-                LSB0_Avg[key + KEY_AMOUNT*nByte] += (
-                (traceList[i][TRACE_NAME] - LSB0_Avg[key + KEY_AMOUNT*nByte]) / (LSB0_Count[key + KEY_AMOUNT*nByte] + 1) )
-                LSB0_Count[key + KEY_AMOUNT*nByte] += 1
+            if getY(plainText, nByte, key) % 2 == 0:
+                lsb0_count[nByte][key] += 1
+                lsb0_avg[nByte][key] += ((trace_now - lsb0_avg[nByte][key]) / lsb0_count[nByte][key])
             else:
-                LSB1_Avg[key + KEY_AMOUNT*nByte] += (
-                (traceList[i][TRACE_NAME] - LSB1_Avg[key + KEY_AMOUNT*nByte]) / (LSB1_Count[key + KEY_AMOUNT*nByte] + 1) )
-                LSB1_Count[key + KEY_AMOUNT*nByte] += 1
-            SUB_LSB1Avg_LSB0Avg[key + KEY_AMOUNT*nByte] = LSB0_Avg[key + KEY_AMOUNT*nByte] - LSB1_Avg[
-                key + KEY_AMOUNT*nByte]
+                lsb1_count[nByte][key] += 1
+                lsb1_avg[nByte][key] += ((trace_now - lsb1_avg[nByte][key]) / lsb1_count[nByte][key])
 
-        for key in range( KEY_AMOUNT ):
-            keyTemp[key + nByte * KEY_AMOUNT] = numpy.amax(abs(SUB_LSB1Avg_LSB0Avg[key + nByte * KEY_AMOUNT]), axis=0)
+            sub_lsb1_lsb0[nByte][key] = lsb1_avg[nByte][key] - lsb0_avg[nByte][key]
+            pointTemp = numpy.amax(abs(sub_lsb1_lsb0[nByte][key]), axis=0)
 
+            if pointTemp > maxPoint:
+                maxPoint = pointTemp
+                maxKey = key
 
-        max = [0.0, 0] * BYTE_AMOUNT
-        for key in range( KEY_AMOUNT ):
-            if (keyTemp[key + nByte * KEY_AMOUNT][0] > max[0]):
-                max[0] = keyTemp[key + nByte * KEY_AMOUNT][0]
-                max[1] = key
+        log_key += ("%02X" % maxKey)
+        print_key += ("%02X " % maxKey)
 
-        log_KEY += ("%02X" % max[1])
-        print_KEY += ("%02X " %max[1])
+    print(str(i) + ': ', print_key)
 
-    print(str(i)+': ',print_KEY)
-
-    E_time = time.time()
+    end_time = time.time()
     f = open(LOG, "a")
-    f.write("round " + str(i) + ": KEY = " + log_KEY + ",")
-    f.write("Cost time = " + str(E_time - S_time) + '\n')
+    f.write("round " + str(i) + ": KEY = " + log_key + ",")
+    f.write("Cost time = " + str(end_time - start_time) + '\n')
     f.close()
-
